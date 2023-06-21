c0419fce29c055a360dce559bb1a76afcc3330b5
Jeison <jeysonm021@gmail.com>
2023-06-21 17:45:18 -0500

    fix for linters

--- cardComponent.js
+++ cardComponent.js
@@ -1,4 +1,49 @@
-import cardsData from './cardData.js';
+const cardsData = [
+  {
+    imgSrc: 'assets/cardImgs/Snapshoot Portfolio.png',
+    title: 'Tonic',
+    projectType: 'Back End Dev',
+    year: '2015',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longDescription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n\nLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.',
+    languages: ['JavaScript', 'HTML', 'CSS', 'Ruby', 'Bootstrap'],
+  },
+  {
+    imgSrc: 'assets/cardImgs/Snapshoot Portfolio-1.png',
+    title: 'Multi-Post Stories',
+    projectType: 'Back End Dev',
+    year: '2015',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longDescription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n\nLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.',
+    languages: ['JavaScript', 'HTML', 'CSS', 'Ruby', 'Bootstrap'],
+  },
+  {
+    imgSrc: 'assets/cardImgs/Snapshoot Portfolio-2.png',
+    title: 'Tonic',
+    projectType: 'Back End Dev',
+    year: '2015',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longDescription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n\nLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.',
+    languages: ['JavaScript', 'HTML', 'CSS', 'Ruby', 'Bootstrap'],
+  },
+  {
+    imgSrc: 'assets/cardImgs/Snapshoot Portfolio-3.png',
+    title: 'Multi-Post Stories',
+    projectType: 'Back End Dev',
+    year: '2015',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longDescription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n\nLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.',
+    languages: ['JavaScript', 'HTML', 'CSS', 'Ruby', 'Bootstrap'],
+  },
+];
 
 //   Get the container element
 const worksContainer = document.getElementById('works_container');
@@ -84,9 +129,6 @@ buttons.forEach((button, index) => {
 
     // Append the modal to the body
     document.body.appendChild(modal);
-    // Append the modal to the div with the class modal_mobile
-    // document.querySelector('.modal_mobile').appendChild(modal);
-
 
     // Show the modal
     modal.showModal();
