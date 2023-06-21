352f39b428c39a7b926b72f1e4ef306e65f2a9ed
Jeison <jeysonm021@gmail.com>
2023-06-21 17:22:07 -0500

    fix for linters

--- cardComponent.js
+++ cardComponent.js
@@ -1,31 +1,31 @@
-import cardsData from "./cardData.js"
+import cardsData from './cardData.js'
 
 
 //   Get the container element
-const worksContainer = document.getElementById("works_container");
+const worksContainer = document.getElementById('works_container');
 
 // Loop through the cards data and create the HTML for each card
 cardsData.forEach((cardData) => {
   // Create the HTML for the card
   const cardHtml = `
-    <div class="cards">
-      <div class="card_img_container">
-        <img src="${cardData.imgSrc}" alt="Project preview image">
+    <div class='cards'>
+      <div class='card_img_container'>
+        <img src='${cardData.imgSrc}' alt='Project preview image'>
       </div>
-      <div class="card_description_container">
+      <div class='card_description_container'>
         <h3>${cardData.title}</h3>
-        <div class="card_projectType_container">
-          <p class="canopy">CANOPY</p>
+        <div class='card_projectType_container'>
+          <p class='canopy'>CANOPY</p>
           <div>●</div>
-          <p class="project_type">${cardData.projectType}</p>
+          <p class='project_type'>${cardData.projectType}</p>
           <div>●</div>
-          <p class="project_year">${cardData.year}</p>
+          <p class='project_year'>${cardData.year}</p>
         </div>
-        <p class="card_description">${cardData.description}</p>
-        <ul class="card_languages">
-          ${cardData.languages.slice(0, 3).map((language) => `<li>${language}</li>`).join("")}
+        <p class='card_description'>${cardData.description}</p>
+        <ul class='card_languages'>
+          ${cardData.languages.slice(0, 3).map((language) => `<li>${language}</li>`).join('')}
         </ul>
-        <button class="card_btn">See project</button>
+        <button class='card_btn'>See project</button>
       </div>
     </div>
   `;
@@ -35,46 +35,46 @@ cardsData.forEach((cardData) => {
 });
 
 // Get the button elements
-const buttons = document.querySelectorAll(".card_btn");
+const buttons = document.querySelectorAll('.card_btn');
 
 // Add event listeners to the button elements
 buttons.forEach((button, index) => {
-  button.addEventListener("click", () => {
+  button.addEventListener('click', () => {
     // Get the card data for the clicked button
     const cardData = cardsData[index];
     console.log(cardData);
 
     // Create the modal element
-    const modal = document.createElement("dialog");
-    modal.classList.add("modal_container");
+    const modal = document.createElement('dialog');
+    modal.classList.add('modal_container');
 
     // Create the HTML for the modal
     const modalHtml = `
-      <div class="modal_content">
-      <div class="modal_title_container">
+      <div class='modal_content'>
+      <div class='modal_title_container'>
         <h3>${cardData.title}</h3>
-        <img class="close_menu" src="assets/Enabled_X.svg" alt="menu x" />
+        <img class='close_menu' src='assets/Enabled_X.svg' alt='menu x' />
       </div>
-        <div class="card_projectType_container">
-          <p class="canopy">CANOPY</p>
+        <div class='card_projectType_container'>
+          <p class='canopy'>CANOPY</p>
           <div>●</div>
-          <p class="project_type">${cardData.projectType}</p>
+          <p class='project_type'>${cardData.projectType}</p>
           <div>●</div>
-          <p class="project_year">${cardData.year}</p>
+          <p class='project_year'>${cardData.year}</p>
         </div>
-        <div class="card_img_container">
-          <img src="${cardData.imgSrc}" alt="Project preview image">
+        <div class='card_img_container'>
+          <img src='${cardData.imgSrc}' alt='Project preview image'>
         </div>
-        <div class="card_description_container">
-          <p class="card_description">${cardData.longDescription.replace(/\n/g, "<br>")}</p>
-          <div class="leftSide_container">
-            <ul class="card_languages">
-              ${cardData.languages.map((language) => `<li>${language}</li>`).join("")}
+        <div class='card_description_container'>
+          <p class='card_description'>${cardData.longDescription.replace(/\n/g, '<br>')}</p>
+          <div class='leftSide_container'>
+            <ul class='card_languages'>
+              ${cardData.languages.map((language) => `<li>${language}</li>`).join('')}
             </ul>
             <hr/>
-            <div class="card_btn_container">
-              <button class="card_btn"><p>See live</p> <img src="assets/seeLive.svg"/></button>
-              <button class="card_btn"><p>See source</p> <img src="assets/GitHub.svg"</button>
+            <div class='card_btn_container'>
+              <button class='card_btn'><p>See live</p> <img src='assets/seeLive.svg'/></button>
+              <button class='card_btn'><p>See source</p> <img src='assets/GitHub.svg'</button>
             </div>
           </div>
         </div>
@@ -87,17 +87,17 @@ buttons.forEach((button, index) => {
     // Append the modal to the body
     document.body.appendChild(modal);
     // Append the modal to the div with the class modal_mobile
-    // document.querySelector(".modal_mobile").appendChild(modal);
+    // document.querySelector('.modal_mobile').appendChild(modal);
 
 
     // Show the modal
     modal.showModal();
 
     // Get the close button element
-    const closeButton = modal.querySelector(".close_menu");
+    const closeButton = modal.querySelector('.close_menu');
 
     // Add event listener to the close button element
-    closeButton.addEventListener("click", () => {
+    closeButton.addEventListener('click', () => {
       // close the modal
       modal.remove();
     });
