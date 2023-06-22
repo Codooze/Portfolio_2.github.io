1440597c967d2b00ae95bb438ec5919ab4122ba6
Jeison <jeysonm021@gmail.com>
2023-06-22 11:17:13 -0500

    fix for linters

--- validations.js
+++ validations.js
@@ -1,21 +1,21 @@
-const form = document.getElementById("contact_form_container");
+const form = document.getElementById('contact_form_container');
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    validateEmail();
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  validateEmail();
 });
 
 function validateEmail() {
-    const emailInput = document.getElementById("email");
-    const emailValue = emailInput.value;
-    const lowercaseEmail = emailValue.toLowerCase();
+  const emailInput = document.getElementById('email');
+  const emailValue = emailInput.value;
+  const lowercaseEmail = emailValue.toLowerCase();
 
-    if (emailValue !== lowercaseEmail) {
-        const errorElement = document.getElementById("error");
-        errorElement.textContent = "The email must be in all lowercase letters.";
-        return false;
-    }
+  if (emailValue !== lowercaseEmail) {
+    const errorElement = document.getElementById('error');
+    errorElement.textContent = 'The email must be in all lowercase letters.';
+    return false;
+  }
 
-    // Email is valid, submit the form or perform other actions
-    return true;
+  // Email is valid, submit the form or perform other actions
+  return true;
 }
